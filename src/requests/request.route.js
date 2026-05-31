ccad5d4bf970d73c29ec2b7d6d1f569eaea2b319
Joy <joykenneth605@gmail.com>
2026-05-31 20:15:30 +0100

    feat: add middleware for async handler

--- src/requests/request.route.js
+++ src/requests/request.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { saveRequest, getRequest, getRequestsWithStatus } from './request.controller.js';
+import { asyncHandler } from '../middleware/asyncHandler.js';
 
 const router = express.Router();
 
-router.post('/request', saveRequest);
+router.post('/request', asyncHandler(saveRequest));
 
-router.get('/requests/:id', getRequest);
+router.get('/requests/:id', asyncHandler(getRequest));
 
-router.get('/requests', getRequestsWithStatus);
+router.get('/requests', asyncHandler(getRequestsWithStatus));
 
 export default router; 
