94487ca8df1de843d78fa4e3a7a99b16a5e12bb1
Joy <joykenneth605@gmail.com>
2026-05-31 14:29:24 +0100

    chore: move requests out of modules folder

--- src/requests/request.service.js
+++ src/requests/request.service.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import db from '../../config/client.js';
+import db from '../config/client.js';
 import { StatusCodes } from 'http-status-codes';
 
 export const storeRequest = async (req, res) => {
